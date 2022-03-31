5bc4327f92a6d46d71f460b24aa1a40823366dd2
FrancoOrtiz <francoportiz@gmail.com>
2022-03-31 12:30:54 -0300

    Sample pokemon request

--- app.js
+++ app.js
@@ -25,48 +25,19 @@ const fetchAllPokemons = async() => {
             img: dataPokemon.sprites.front_default,
         }
     }))
-    console.log(data);
+    return data;
 }
 
-fetchAllPokemons();
+let allPokemons = [];
+fetchAllPokemons().then((response) => allPokemons = response).finally(() => {console.log('allPokemons log')});
 
 app.get('/api/:pokemon', async(req, res) => {
 
-    // const pokemon = req.params.pokemon;
-    // const peticion = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    // const namedPokemon = await peticion.json();
-    // const dataPokemon = await [{
-    //     name: namedPokemon.name,
-    //     img: namedPokemon.sprites.front_default,
-    //     order: namedPokemon.order
-    // }]
-    // res.send(dataPokemon);
+    const pokemon = req.params.pokemon;
+    
 })
 
 app.get('/api/pokemons/:page',(req, res) => {
-
-    // const offset = req.params.page * 3;
-    // const displayPokemons = [];
-    
-    // for(let i=offset; i > offset - 4; i--){
-    //     displayPokemons.push(data[i]);
-    // }
-   
-    // return displayPokemons;
-
-    // const peticion = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=3&offset=${offset}`)
-    // const allPokemons = await peticion.json();
-    // const data = await Promise.all(allPokemons.results.map(async(pokemon) => {
-    //     const url = await fetch(pokemon.url)
-    //     const dataPokemon = await url.json()
-    //     return await {
-    //         name: dataPokemon.name,
-    //         id: dataPokemon.id,
-    //         types: dataPokemon.types,
-    //         forms: dataPokemon.forms,
-    //         order: dataPokemon.order,
-    //         img: dataPokemon.sprites.front_default,
-    //     }
-    // })) 
-    // res.send(data)
+    const offset = req.params.page * 3;
+    res.send(allPokemons.slice(offset, offset + 3));
 })
