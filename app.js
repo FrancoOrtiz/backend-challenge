37ebb0bc8bd9b54cce5025d3ec60c6536bf8726c
FrancoOrtiz <francoportiz@gmail.com>
2022-04-01 17:38:14 -0300

    Code cleanup

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { pokemonByPage, pokemonByName } from "./actions/actions.js";
+
+const app = express();
+
+app.use(cors());
+
+app.listen(3002, () => {
+  console.log("server running on port", 3002);
+});
+
+// Search by Name
+
+app.get("/api/:pokemon", pokemonByName);
+
+// Pokemons to show
+
+app.get("/api/pokemons/:page", pokemonByPage);
